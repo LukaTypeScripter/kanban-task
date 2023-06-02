5be2bfa6025a63c42360852abd57c49de80a8a49
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-02 18:01:30 +0400

    default styles.

---
+++ src/RootStyles.ts
@@ -0,0 +1,18 @@
+
+
+export const DefaultStyles = {
+    colors: {
+        MainPurple: "#635FC7",
+        MainPurpleHover: "#A8A4FF",
+        Black:"#000112",
+        VeryDarkGray:"#20212C",
+        DarkGray:'#2B2C37',
+        LinesDark:'#3E3F4E',
+        MediumGray:"#828FA3",
+        LinesLight:'#E4EBFA',
+        LigthGray:"#F4F7FD",
+        White:'#FFFFFF',
+        Red:'#EA5555',
+        RedHover:"#FF9898"
+    }
+}
