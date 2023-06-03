51c270fc26fc65285ddad071dcf462a05dec9361
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-03 16:49:10 +0400

    new task styles and component modal

---
+++ src/components/AddNewTask/AddNewTask.tsx
@@ -0,0 +1,88 @@
+import React, { useContext, useEffect, useRef } from "react";
+import {
+  Modal,
+  NewBoardHeader,
+  Lables,
+  InputCont,
+  BoardNameInp,
+  ModalCOnt,
+  ModalCols,
+  ModalCol,
+  AddBtn,
+  SecondBtn,
+} from "../AddNewBoard/NewBoardStyles/newBoardStyles";
+import AppContext from "../../contexts/Header";
+import {
+  DescCont,
+  Description,
+  OptSelection,
+  Options,
+  SelectCont,
+} from "./NewTaskStyles/newTask";
+import cross from "../../assets/icon-cross.svg";
+function AddNewTask() {
+    const {
+        isToggled,
+        handleDeleteSubTasks,
+        setSubTasks,
+        subTasks,
+        handleAddSubTask,
+        setIsNewTask
+
+      } = useContext(AppContext);
+   
+
+  return (
+    <ModalCOnt>
+      <Modal isToggled={isToggled} >
+        <NewBoardHeader isToggled={isToggled}>
+            Add New Task
+            <img src={cross} alt="" onClick={() => setIsNewTask(false)} />
+        </NewBoardHeader>
+        <Lables isToggled={isToggled}>Task Name</Lables>
+        <InputCont>
+          <BoardNameInp isToggled={isToggled} type="text" />
+        </InputCont>
+        <DescCont>
+        <Lables isToggled={isToggled}>Description</Lables>
+          <Description isToggled={isToggled} />
+        </DescCont>
+        <Lables isToggled={isToggled}>SubTasks</Lables>
+        <ModalCols>
+          {subTasks.map((input, index) => (
+            <ModalCol>
+              <InputCont>
+                <BoardNameInp
+                  isToggled={isToggled}
+                  key={index}
+                  type="text"
+                  value={input}
+                  onChange={(e) => {
+                    const newInputs = [...subTasks];
+                    newInputs[index] = e.target.value;
+                    setSubTasks(newInputs);
+                  }}
+                />
+              </InputCont>
+              <img src={cross} onClick={() => handleDeleteSubTasks(index)} />
+            </ModalCol>
+          ))}
+          <AddBtn onClick={handleAddSubTask} isToggled={isToggled}>
+            + Add SubTask
+          </AddBtn>
+        </ModalCols>
+        <SelectCont>
+          <Lables isToggled={isToggled}>Current Status</Lables>
+          <Options isToggled={isToggled}>
+            <OptSelection>Todo</OptSelection>
+            <OptSelection>Doing</OptSelection>
+            <OptSelection>Done</OptSelection>
+          </Options>
+        </SelectCont>
+        <SecondBtn>Create Task</SecondBtn>
+      </Modal>
+    </ModalCOnt>
+  );
+}
+
+export default AddNewTask;
---
+++ src/components/AddNewTask/NewTaskStyles/newTask.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { DefaultStyles } from "../../../RootStyles";
+import arrowDown from '../../../assets/icon-chevron-down.svg'
+const {MediumGray,DarkGray,White} = DefaultStyles.colors
+export const Description = styled.textarea<{isToggled:boolean}>`
+
+margin-bottom: 24px;
+border: 1px solid rgba(130,143,163,.25);
+    border-radius: 4px;
+    font-size: 13px;
+    font-weight: 500;
+    height: 40px;
+    padding: 8px 16px;
+    width: 100%;
+    height: 135px;
+    resize: none;
+    background-color: ${props => props.isToggled ? '' : DarkGray};
+`
+
+
+
+export const SelectCont = styled.div`
+margin-top: 24px;
+`
+
+export const DescCont = styled.div`
+margin-top: 24px;
+`
+
+export const Options = styled.select<{isToggled:boolean}>`
+appearance: none;
+width: 100%;
+background-image: url(${arrowDown});
+background-position: right 16px top 50%;
+background-repeat: no-repeat;
+border: 1px solid #828fa340;
+    border-radius: 4px;
+    padding: 8px 16px;
+background-color: ${props => props.isToggled ? "" : DarkGray};
+color: ${props => props.isToggled ? "" : White};
+`
+
+export const OptSelection = styled.option`
+    color:${MediumGray};
+`
