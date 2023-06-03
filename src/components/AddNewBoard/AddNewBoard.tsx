f7fb3fc49317ce02b0c213156b60e69a8a78c849
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-03 16:48:48 +0400

    new Board styles and component modal

---
+++ src/components/AddNewBoard/AddNewBoard.tsx
@@ -0,0 +1,68 @@
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { Modal, ModalCOnt,NewBoardHeader,Lables,BoardNameInp,AddBtn,ModalCols,ModalCol, InputCont, SecondBtn } from './NewBoardStyles/newBoardStyles'
+import AppContext from '../../contexts/Header';
+import cross from '../../assets/icon-cross.svg'
+import AddNewTask from '../AddNewTask/AddNewTask';
+
+function AddNewBoard() {
+    const {setIsOpenAddModal,columnInputs,setColumnInputs,handleAddColumnInput,handleDeleteColumnInput,isToggled} = useContext(AppContext)
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const handleOutsideClick = (event:any) => {
+        if (modalRef.current && !modalRef.current.contains(event.target)) {
+            setIsOpenAddModal(false);
+        }
+      };
+  
+      document.addEventListener('mousedown', handleOutsideClick);
+  
+      return () => {
+        document.removeEventListener('mousedown', handleOutsideClick);
+      };
+    }, []);
+  return (
+    <ModalCOnt>
+        <Modal ref={modalRef} isToggled={isToggled}>
+            <NewBoardHeader isToggled={isToggled}>Add new Board</NewBoardHeader>
+            <Lables isToggled={isToggled}>Board Name</Lables>
+            <BoardNameInp isToggled={isToggled}  type='text' />
+            <Lables isToggled={isToggled}>Board Columns</Lables> 
+            <ModalCols>
+            {columnInputs.map((input,index) => (
+                <ModalCol>
+                 <InputCont>   
+    <BoardNameInp
+    isToggled={isToggled} 
+    key={index}
+    type='text'
+    value={input}
+    onChange={(e) => {
+        const newInputs = [...columnInputs]
+        newInputs[index] = e.target.value
+        setColumnInputs(newInputs)
+    }}
+    />
+    </InputCont>
+        <img src={cross} onClick={() => handleDeleteColumnInput(index)} />
+    </ModalCol>
+))}
+
+            </ModalCols>
+            <AddBtn onClick={handleAddColumnInput} isToggled={isToggled} >+ Add column</AddBtn>
+            <SecondBtn onClick={handleAddColumnInput}>Create New Board</SecondBtn>
+            
+        </Modal>
+      {/**add New task modal */}
+      
+
+    </ModalCOnt>
+  )
+}
+
+export default AddNewBoard
+
+
+
+
+
---
+++ src/components/AddNewBoard/NewBoardStyles/newBoardStyles.ts
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+import { DefaultStyles } from "../../../RootStyles";
+const {DarkGray,White,MediumGray,Black,MainPurple} = DefaultStyles.colors
+export const ModalCOnt = styled.div`
+    align-items: center;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    left: 0;
+    position: fixed;
+    right: 0;
+    top: 0;
+    z-index: 9;
+    background-color: rgba(0,0,0,.5);
+`
+
+
+export const Modal = styled.div <{isToggled:boolean}>`
+background-color: ${props => props.isToggled ?  White : DarkGray };
+border-radius: 6px;
+max-height: 90%;
+overflow-y: scroll;
+padding: 24px;
+width: 343px;
+`
+
+
+export const NewBoardHeader = styled.h3 <{isToggled:boolean}>`
+ margin-bottom: 24px;
+ color: ${props => props.isToggled ? "" : White};
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+`
+
+export const  Lables = styled.label<{isToggled:boolean}>`
+display: block;
+color: ${props => props.isToggled ? MediumGray: White};
+font-weight: 700;
+font-size: 12px;
+line-height: 15px;
+margin-bottom: 8px;
+`
+
+export const BoardNameInp = styled.input<{isToggled:boolean}>`
+border: 1px solid rgba(130, 143, 163, 0.25);
+border-radius: 4px;
+font-size: 15px;
+font-weight: 500;
+height: 40px;
+padding: 8px 16px;
+width: 100%;
+text-transform: capitalize;
+color: ${props => props.isToggled ? Black : White};
+background-color: ${props => props.isToggled ? "" : DarkGray};
+`
+export const ModalCols = styled.div`
+
+`
+
+
+export const ModalCol = styled.div`
+display: flex;
+gap:16px;
+align-items: center;
+margin-bottom: 12px;
+`
+export const InputCont = styled.div`
+    position: relative;
+    width: 100%;
+`
+
+
+
+
+
+
+export const AddBtn = styled.button<{isToggled:boolean}>`
+    align-items: center;
+    display: flex;
+    font-size: 13px;
+    height: 40px;
+    justify-content: center;
+    width: 100%;
+    background-color: ${props => props.isToggled ? MainPurple: White};
+    border-radius: 24px;
+    color: ${props => props.isToggled ? White: MainPurple};
+    font-size: 14px;
+    font-weight: 700;
+    margin-top: 24px;
+    padding: 15px 18px;
+    outline: none;
+    border: none;
+`
+export const SecondBtn = styled.button`
+ align-items: center;
+    display: flex;
+    font-size: 13px;
+    height: 40px;
+    justify-content: center;
+    width: 100%;
+    background-color: ${MainPurple};
+    border-radius: 24px;
+    color:${White};
+    font-size: 14px;
+    font-weight: 700;
+    margin-top: 24px;
+    padding: 15px 18px;
+    outline: none;
+    border: none;
+
+`
