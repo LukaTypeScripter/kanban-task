7d0a894b8c34ff45191976ab3389d2c3525a605a
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-04 20:19:38 +0400

    added new modals

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,30 +9,38 @@ import AppContext from '../../contexts/Header'
 import NavModal from '../NavModal/NavModal'
 import AddNewBoard from '../AddNewBoard/AddNewBoard'
 import AddNewTask from '../AddNewTask/AddNewTask'
+import MainPage from '../Main/MainPage'
+import Data from '../../data.json'
+import MainContext from '../../contexts/MainContext'
+import Elapsis from '../Elapsis-menu/Elapsis'
 function Header() {
-   const {toggleOpen,isOpen,isToggled,isOpenAddModal,handleTaskModal,isNewTask} = useContext(AppContext)
+   const {toggleOpen,isOpen,isToggled,isOpenAddModal,handleTaskModal,isNewTask,isSmallModalOpen,setIsSmallModalOpen} = useContext(AppContext)
+   const {selectedPlatform} = useContext(MainContext)
   return (
     <>
    <HeaderCont isToggled={isToggled}>
     <Imgs src={logo}/>
     <HeaderName isToggled={isToggled}>
-    <h3>Platform Launch</h3>
+    <h3>{selectedPlatform}</h3>
     <img onClick={toggleOpen} src={isOpen ?ArrowUp : ArrowDOwn} alt="" />
     </HeaderName>
     <AddBtn><img  src={AddTask} alt="" onClick={handleTaskModal} /></AddBtn>
-    <VerticalImg src={ThreeDots} alt="" />
+    <VerticalImg src={ThreeDots} onClick={() => setIsSmallModalOpen(!isSmallModalOpen)} alt="" />
     {isOpen && (
     <NavModal/>
    )}
    {isOpenAddModal && (
     <AddNewBoard/>
    )}
-   
+   {isSmallModalOpen && (
+ <Elapsis/>
+   )}
    </HeaderCont>
    {isNewTask && (
     <AddNewTask/>
    )}
-
+  
+  <MainPage/>
    </>
   )
 }
