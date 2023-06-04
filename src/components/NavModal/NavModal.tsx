9f404c2ed8c1527bedcfc82211a3b0ffd735bb63
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-04 20:21:10 +0400

    added logic to close modal with esc and  out click

--- src/components/NavModal/NavModal.tsx
+++ src/components/NavModal/NavModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 
 import { DropDownModal,DropDownContainer,Title,DropDownBoards,DropDownBoard,BoardIcon,AddNewTask,AddBoardIcon,AddBoardText,TogglerTheme,Switch,Inputs,Slider } from './NavModalStyles/navModal'
 import NameData from '../../data.json'
@@ -6,15 +6,43 @@ import IconBoard from '../../assets/icon-board.svg'
 import ligthTheme from '../../assets/icon-light-theme.svg'
 import darkTheme from '../../assets/icon-dark-theme.svg'
 import AppContext, { AppContextProvider } from '../../contexts/Header'
+import MainContext from '../../contexts/MainContext'
 function NavModal() {
     const [activeIndex, setActiveIndex] = useState<number | null>(null);
+   const modalRef = useRef<HTMLDivElement>(null)
+    const {toggleTheme,isToggled,toggleAddNewBBOard,setIsOpen} = useContext(AppContext)
+    const {HandlePlatformChange} = useContext(MainContext)
     const handleItemClick = (index: number) => {
-        setActiveIndex(index);
-      };
-    const {toggleTheme,isToggled,toggleAddNewBBOard} = useContext(AppContext)
+      setActiveIndex(index);
+      const selectedBoard = NameData.boards[index].name;
+      HandlePlatformChange(selectedBoard);
+      
+    };
+
+    //close when you click outside the container
+    useEffect(() => {
+      const handleOutsideClick = (e:any) => {
+        if(modalRef.current && !modalRef.current.contains(e.target)) {
+          setIsOpen(false)
+        }
+
+      }
+      const handleEscClick = (e:any) => {
+        if(e.key === 'Escape') {
+          setIsOpen(false)
+        }
+      }
+      document.addEventListener('mousedown', handleOutsideClick)
+      document.addEventListener('keydown', handleEscClick)
+    
+      return () => {
+        document.removeEventListener('mousedown', handleOutsideClick)
+      document.removeEventListener('keydown', handleEscClick)
+      }
+    },[])
   return (
-    <DropDownContainer >
-        <DropDownModal isToggled={isToggled}>
+    <DropDownContainer  >
+        <DropDownModal isToggled={isToggled} ref={modalRef}>
             <Title>ALL BOARDS (3)</Title>
             <DropDownBoards>
                {NameData.boards.map((board, index) => (
