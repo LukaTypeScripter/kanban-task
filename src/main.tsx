9df40ebad972267e949868b695ee3f9648c0783d
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-04 20:18:57 +0400

    wrap app wiuth context

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AppContextProvider } from './contexts/Header.tsx'
+import {MainContextProvider} from './contexts/MainContext.tsx'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AppContextProvider>
+      <MainContextProvider>
     <App />
+    </MainContextProvider>
     </AppContextProvider>
   </React.StrictMode>,
 )
