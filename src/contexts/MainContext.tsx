189e4742882dcb760a7ab1275fc71980c6c009a0
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-05 15:17:27 +0400

    added some use states for for future use

--- src/contexts/MainContext.tsx
+++ src/contexts/MainContext.tsx
@@ -1,18 +1,47 @@
 
 import React, { createContext, useState } from "react";
+import NameData from '../data.json'
+interface Task {
+  title: string;
+  description: string;
+  status: string;
+  subtasks: SubTask[];
+}
 
+interface SubTask {
+  title: string;
+  isCompleted: boolean;
+}
+
+interface Column {
+  name: string;
+  tasks: Task[];
+}
+
+interface BoardData {
+  name: string;
+  columns: Column[];
+}
 interface MainContextValue {
   HandlePlatformChange: (platform: string) => void;
   selectedPlatform: string;
   setName:React.Dispatch<React.SetStateAction<string>>,
-  name:string
+  name:string,
+  boardName:string,
+  setBoardName:React.Dispatch<React.SetStateAction<string>>
+  boardData:BoardData[],
+  setBoaredData:React.Dispatch<React.SetStateAction<BoardData[]>>;
 }
 
 const initialContextValue: MainContextValue = {
   HandlePlatformChange: () => {},
   selectedPlatform: "Platform Launch",
   setName: () => {},
-  name:''
+  name:'',
+  boardName:"",
+  setBoardName:() => {},
+  boardData: [] as BoardData[],
+  setBoaredData:() => {},
 };
 
 const MainContext = createContext<MainContextValue>(initialContextValue);
@@ -24,7 +53,8 @@ export const MainContextProvider = ({ children }: {
     "Platform Launch"
   );
   const [name,setName] = useState<string>("");
-
+  const [boardName,setBoardName] = useState<string>("")
+  const [boardData,setBoaredData] = useState(NameData.boards)
   const HandlePlatformChange = (platform: string) => {
     setSelectedPlatform(platform);
   };
@@ -33,7 +63,11 @@ export const MainContextProvider = ({ children }: {
     selectedPlatform,
     HandlePlatformChange,
     setName,
-    name
+    name,
+    boardName,
+    setBoardName,
+    boardData,
+    setBoaredData
   };
 
   return (
