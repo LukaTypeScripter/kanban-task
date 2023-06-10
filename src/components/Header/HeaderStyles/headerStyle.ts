e67c8e00938638f587c988eec72f091af92967f8
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-10 20:47:47 +0400

    added some header styles and made things respnsive

--- src/components/Header/HeaderStyles/headerStyle.ts
+++ src/components/Header/HeaderStyles/headerStyle.ts
@@ -10,11 +10,49 @@ display: flex;
 padding:  16px;
 background-color: ${props => props.isToggled ? "" : DarkGray};
 `
+export const LogoCont = styled.div`
 
+@media only screen and (min-width: 768px){
+    align-items: center;
+    display: flex;
+    gap: 15px;
+    padding-left: 10px;
+    width: 245px;
+   
+}
+
+
+`
+export const ProjectName = styled.h3`
+
+@media only screen and (min-width: 768px){
+    font-size: 32px;
+    font-weight: 700;
+
+}
+@media only screen and (max-width: 768px){
+    display:none;
+
+}
+`
 export const Imgs = styled.img`
 
 `
+export const ImgsPlus = styled.img`
+    @media only screen and (min-width: 768px){
+    display:none;
 
+}
+`
+export const AddNewTasks = styled.span`
+    font-size: 15px;
+    font-weight: 700;
+    line-height: 19px;
+    @media only screen and (max-width: 768px){
+    display:none;
+
+}
+`
 export const HeaderName = styled.div<{isToggled:boolean}>`
 align-items: center;
 display: flex;
@@ -22,6 +60,12 @@ gap: 8px;
 margin-left: 16px;
 margin-right: auto;
 color: ${props => props.isToggled ? "" : White};
+img {
+    @media only screen and (min-width: 768px){
+    display:none;
+
+}
+}
 `
 export const AddBtn = styled.button`
 align-items: center;
@@ -37,11 +81,22 @@ border:none;
 &:hover {
     background-color: ${DefaultStyles.colors.MainPurpleHover};
 }
+@media only screen and (min-width: 768px){
+    height: 48px;
+    margin-right: 24px;
+    width: 164px;
+
+}
 `
 export const VerticalImg = styled.img`
 height: 16px;
 cursor: pointer;
 margin-top: 8px;
+@media only screen and (min-width: 768px){
+    height: 20px;
+    width: 5px;
+
+}
 `
 export const AddToDos = styled.div`
 
