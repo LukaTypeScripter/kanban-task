9dfcf7a13350ecb565031d40f45e704556b50bff
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-10 20:48:48 +0400

    Task Del elkapsis

---
+++ src/components/TaskDelateElapsis/TaskDElateElapsis.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef,useContext } from 'react'
+import { Delate } from '../Elapsis-menu/ElapsisStyles/elapsis'
+import AppContext from '../../contexts/Header'
+import { ElapsisMenuTask } from './styles/taskDel'
+import MainContext from '../../contexts/MainContext'
+
+function TaskDElateElapsis() {
+    const smallModalREf = useRef<HTMLDivElement>(null)
+    const {isToggled,} = useContext(AppContext)
+    const {setIsOpenTaskDel,isOpenTaskDel,setIsOpenAboutModal,setIsOpenDelElapsis} = useContext(MainContext)
+    useEffect(() => {
+      const handleOutsideClick = (event: MouseEvent) => {
+        if (smallModalREf.current && !smallModalREf.current.contains(event.target as Node)) {
+          
+        }
+      };
+  
+      document.addEventListener('mousedown', handleOutsideClick);
+  
+      return () => {
+        document.removeEventListener('mousedown', handleOutsideClick);
+      };
+    })
+
+    const toggle= () => {
+      setIsOpenAboutModal(false)
+      setIsOpenTaskDel(!isOpenTaskDel)
+      setIsOpenDelElapsis(false)
+    }
+  
+  return (
+    <ElapsisMenuTask ref={smallModalREf} isToggled={isToggled}>
+        <Delate onClick={toggle}>Delate board</Delate>
+    </ElapsisMenuTask>
+  )
+}
+
+export default TaskDElateElapsis
