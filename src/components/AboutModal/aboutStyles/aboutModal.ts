80c14d4d987a53af3e418c28d6c0f161ddeff4bf
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-06-12 18:40:14 +0400

    some styling

--- src/components/AboutModal/aboutStyles/aboutModal.ts
+++ src/components/AboutModal/aboutStyles/aboutModal.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-
+import { DefaultStyles } from "../../../RootStyles";
+const { Black,White,VeryDarkGray} = DefaultStyles.colors
 
 export const TaskModalCOnt = styled.div`
 align-items: center;
@@ -10,31 +11,32 @@ margin-bottom: 24px;
 position: relative;
 `
 
-export const Title = styled.p`
+export const Title = styled.p<{isToggled:boolean}>`
     font-size: 18px;
     font-weight: 700;
     line-height: 23px;
+    color: ${props => props.isToggled ? "" : White};
 `
 
-export const Desc = styled.p`
-    color: #828fa3;
+export const Desc = styled.p<{isToggled:boolean}>`
+    color: ${props => props.isToggled ? "#828fa3" : White};
     margin-bottom: 24px;
     font-size: 13px;
     font-weight: 500;
     line-height: 23px;
 `
 
-export const SubTasksCompleted = styled.p`
-    color: #828fa3;
+export const SubTasksCompleted = styled.p<{isToggled:boolean}>`
+    color: ${props => props.isToggled ? "#828fa3" : White};
     margin-bottom: 16px;
     font-size: 12px;
     font-weight: 700;
     letter-spacing: 2.4px;
     line-height: 15px;
 `
-export const Subtasks = styled.div`
+export const Subtasks = styled.div<{isToggled:boolean}>`
     align-items: center;
-    background: #f4f7fd;
+    background: ${props => props.isToggled ? "#f4f7fd" :VeryDarkGray };
     border-radius: 4px;
     display: flex;
     gap: 16px;
@@ -48,17 +50,19 @@ export const SubTasksCheckBox = styled.input`
         accent-color: #635fc7; 
     }
 `
-export const Todo = styled.p`
+export const Todo = styled.p<{isToggled:boolean}>`
     font-size: 12px;
     font-weight: 700;
     line-height: 15px;
+    color: ${props => props.isToggled ? "" : White};
 `
 
 export const SelectCont = styled.div`
     margin-top: 24px;
 `
-export const Status = styled.label`
+export const Status = styled.label<{isToggled:boolean}>`
     font-size: 12px;
     font-weight: 700;
     line-height: 15px;
+    color: ${props => props.isToggled ? "" : White};
 `
